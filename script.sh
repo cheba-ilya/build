99784c2fbb8560d7f37e0c932aafd1ee4f182b35
cheba-ilya <58446724+cheba-ilya@users.noreply.github.com>
2020-03-06 16:51:34 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "chek"
+echo "lok"
 sudo apt-get update 
 sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-echo "chek"
-docker pull artmin/minaev-doc
-docker run -d -p 80:80 artmin/minaev-doc
+echo "lok"
+docker pull cheba2011/neslojno
+docker run -d -p 8080:8080 cheba2011/neslojno
