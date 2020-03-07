2567b15812b3ee6ef6fb8d20dfff3942bceea40f
cheba-ilya <58446724+cheba-ilya@users.noreply.github.com>
2020-03-07 21:19:29 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt -y update
-sudo apt -y install httpd
+sudo apt -y install apache2
 myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
 sudo echo "<h1>Web Server with ip: $myip</h1><br>Build by Terraform!<br>Use bash to have more" > /var/www/html/index.html
 sudo service httpd start
