e70715e302a1b563f2afe7cd6445617b2d124d99
cheba-ilya <58446724+cheba-ilya@users.noreply.github.com>
2020-03-07 17:50:51 +0200

    changedscr

--- script.sh
+++ script.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-echo "lok"
-sudo apt-get update 
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-echo "lok"
-docker pull cheba2011/neslojno
-docker run -d -p 8080:8080 cheba2011/neslojno
+apt -y update
+apt -y install httpd
+echo "<h1>Web Server with ip: $myip</h1><br>Build by Terraform!<br>Use bash to have more" > /var/www/html/index.html
+sudo service httpd start
+chkconfig httpd on
