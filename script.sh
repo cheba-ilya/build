b93c23ba8bc8c8467a17c88eda37e8e919da76a4
cheba-ilya <58446724+cheba-ilya@users.noreply.github.com>
2020-03-07 20:32:14 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt -y update
 apt -y install httpd
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
 echo "<h1>Web Server with ip: $myip</h1><br>Build by Terraform!<br>Use bash to have more" > /var/www/html/index.html
 sudo service httpd start
 chkconfig httpd on
